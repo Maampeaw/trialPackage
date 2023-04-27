0ba6eba1566ced4e02f3b352ff1ffa77358d6290
MarkAmoah <markamoah97@gmail.com>
2023-04-27 17:43:30 +0000

    added a view Controller

---
+++ Sources/CustomCheckoutPackage/CheckoutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CheckoutViewController.swift
+//  
+//
+//  Created by Mark Amoah on 4/27/23.
+//
+
+import UIKit
+
+public class CheckoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+
+    
+
+}
