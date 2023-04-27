77bd4b0a1cdf53beb77badb1a65b2480479b7964
MarkAmoah <markamoah97@gmail.com>
2023-04-27 17:54:01 +0000

    add static function to custom controller to open controller

--- Sources/CustomCheckoutPackage/CheckoutViewController.swift
+++ Sources/CustomCheckoutPackage/CheckoutViewController.swift
@@ -8,13 +8,14 @@
 import UIKit
 
 public class CheckoutViewController: UIViewController {
-
+    public static func openController(with customController: UIViewController){
+        let controller = CheckoutViewController()
+        customController.present(controller, animated: true)
+    }
     public override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
     }
-    
-
     
 
 }
